03435b268c5cf603583e74c989b0c48ad19b9c48
IxiAngel <33629337+IxiAngel@users.noreply.github.com>
2025-11-27 19:04:20 +0100

    qxc-0.9.1c

--- QuIXI/Meta/Config.cs
+++ QuIXI/Meta/Config.cs
@@ -10,7 +10,7 @@ namespace QuIXI.Meta
     public class Config
     {
         // Read-only values
-        public static readonly string version = "qxc-0.9.1b";
+        public static readonly string version = "qxc-0.9.1c";
 
         public static readonly string checkVersionUrl = "https://resources.ixian.io/qxc-update.txt";
         public static readonly int checkVersionSeconds = 6 * 60 * 60; // 6 hours
